349567e289ba65692eed36622ce8857f023f900a
Jemma <git@jemsoftware.dev>
2022-08-24 16:59:13 -0500

    moved notifications render to be async in order to support database calls for dynamic notification content.

--- src/services/juxt-web/routes/console/notifications.js
+++ src/services/juxt-web/routes/console/notifications.js
@@ -1,17 +1,24 @@
 var express = require('express');
+const ejs = require('ejs');
 const database = require('../../../../database');
 const config = require('../../../../../config.json');
+const path = require('node:path');
 var moment = require('moment');
 var router = express.Router();
 
 router.get('/', async function (req, res) {
     let notifications = await database.getNotifications(req.pid, 25, 0);
-    res.render(req.directory + '/notifications.ejs', {
+    let directory = path.join(__dirname, '../../../../webfiles', req.directory);
+    const html = await ejs.renderFile(directory + '/notifications.ejs', {
         moment: moment,
         notifications: notifications,
         cdnURL: config.CDN_domain,
-        lang: req.lang
-    });
+        lang: req.lang,
+        database: database
+    },
+        {async: true}
+    );
+    res.send(html);
     notifications.filter(noti => noti.read === false).forEach(function(notification) {
         notification.markRead();
     });
