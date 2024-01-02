c047aa86bdc6f6de22d001c8ca3b3f20b7a2e3fd
Jemma <git@jemsoftware.dev>
2024-01-02 13:40:46 -0600

    Fixed ban messages on the mobile site not being displayed.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -65,13 +65,28 @@ async function auth(request, response, next) {
 		// This includes ban checks for both Juxt specifically and the account server, ideally this should be squashed
 		// assuming we support more gradual bans on PNID's
 		if (user && (user.account_status < 0 || user.account_status > 1 || request.user.accessLevel < 0)) {
-			response.render(request.directory + '/partials/ban_notification.ejs', {
-				user: user,
-				moment: moment,
-				cdnURL: config.CDN_domain,
-				lang: request.lang,
-				pid: request.pid
-			});
+			if (includes(request, 'juxt')) {
+				let banMessage = '';
+				switch (user.account_status) {
+					case 2:
+						banMessage = `${request.user.username} has been banned until: ${ moment(user.ban_lift_date) }. \n\nReason: ${user.ban_reason}. \n\nIf you have any questions contact the developers in the Discord server.`;
+						break;
+					case 3:
+						banMessage = `${request.user.username} has been banned forever. \n\nReason: ${user.ban_reason}. \n\nIf you have any questions contact the developers in the Discord server.`;
+						break;
+					default:
+						banMessage = `${request.user.username} has been banned. \n\nIf you have any questions contact the developers in the Discord server.`;
+				}
+				return response.render('web/login.ejs', {toast: banMessage, cdnURL: config.CDN_domain,});
+			} else {
+				response.render(request.directory + '/partials/ban_notification.ejs', {
+					user: user,
+					moment: moment,
+					cdnURL: config.CDN_domain,
+					lang: request.lang,
+					pid: request.pid
+				});
+			}
 		}
 	}
 
