c8aa8970392b45d2582ca2e5a14f1042bb0616f9
Jemma Poffinbarger <git@jemsoftware.dev>
2025-01-20 15:25:37 -0600

    chore: cleanup old timing debug statements

--- src/middleware/detectVersion.js
+++ src/middleware/detectVersion.js
@@ -2,7 +2,6 @@ const util = require('../util');
 
 async function detectVersion(request, response, next) {
 	request.timerDate = Date.now();
-	console.time(`Time Request ${request.timerDate}`);
 	// Check the domain and set the directory
 	if (includes(request, 'juxt')) {
 		request.directory = 'web';
